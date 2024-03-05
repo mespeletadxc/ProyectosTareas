b33a2b743bdee0da6ec9a017f6afd6b92e16d35b
mespeletadxc <maria.espeleta@dxc.com>
2024-03-05 22:08:23 +0100

    src

--- src/main/java/com/banana/proyectostareas/model/Proyecto.java
+++ src/main/java/com/banana/proyectostareas/model/Proyecto.java
@@ -1,16 +1,30 @@
 package com.banana.proyectostareas.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "proyects")
 public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 10)
     private String nombre;
+    @DateTimeFormat
+    @NotNull
     private LocalDate fechaCreacion;
 }
--- src/main/java/com/banana/proyectostareas/model/Tarea.java
+++ src/main/java/com/banana/proyectostareas/model/Tarea.java
@@ -1,19 +1,33 @@
 package com.banana.proyectostareas.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
 public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String descripcion;
+    @DateTimeFormat
     private LocalDate fechaLimite;
+    @NotNull
+    @Min(0)
     private Integer orden;
+    @NotNull
     private boolean completada;
 
 }
