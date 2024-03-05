373c7014f2197e022ff030ea26374da0f08d94cb
mespeletadxc <maria.espeleta@dxc.com>
2024-03-05 21:35:40 +0100

    estructura

---
+++ src/main/java/com/microcompany/proyectostareas/ProyectosTareas/ProyectosTareasApplication.java
@@ -0,0 +1,13 @@
+package com.microcompany.proyectostareas.ProyectosTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectosTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectosTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microcompany/proyectostareas/ProyectosTareas/ProyectosTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcompany.proyectostareas.ProyectosTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
